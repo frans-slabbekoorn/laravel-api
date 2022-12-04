3a976bce12764ff591a34520c98f816e0edee5a5
Frans Slabbekoorn <fransslab2003@gmail.com>
2022-12-04 15:22:25 +0100

    Remove ResponseFormatter trait and add Json class for returning formatted json responses.

--- app/Helpers/Json.php
+++ app/Helpers/Json.php
@@ -1,17 +1,18 @@
 <?php
 
-namespace App\Traits;
+namespace App\Helpers;
 
 use Illuminate\Http\JsonResponse;
 
-trait ResponseFormatter
+class Json
 {
     /**
      * Create a new JSON success response instance.
      *
+     * @param string $message
      * @param int $options
      */
-    protected static function success(mixed $data = [], string $message = null, int $status = 200, array $headers = [], $options = 0): JsonResponse
+    public static function success(mixed $data = [], string $message = null, int $status = 200, array $headers = [], $options = 0): JsonResponse
     {
         return response()->json([
             'success' => true,
@@ -20,7 +21,12 @@ trait ResponseFormatter
         ], $status, $headers, $options);
     }
 
-    protected function error(mixed $errors = [], string $message = null, int $code = 422): JsonResponse
+    /**
+     * Create a new JSON error response instance.
+     *
+     * @param string $message
+     */
+    public static function error(mixed $errors = [], string $message = null, int $code = 422): JsonResponse
     {
         return response()->json([
             'success' => false,
